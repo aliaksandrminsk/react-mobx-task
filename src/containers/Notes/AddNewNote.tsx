69609fcfeff6bc4ccb7382acdc5624884b2cda34
Aliaksandr <gadzinik@gmail.com>
2022-03-25 15:06:44 +0300

    refactoring

--- src/containers/Notes/AddNewNote.tsx
+++ src/containers/Notes/AddNewNote.tsx
@@ -1,18 +1,24 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { INote, NoteStore } from "../../store/NoteStore";
+import { observer, useLocalObservable } from "mobx-react-lite";
 
 export const AddNewNote: React.FC<{ noteStore: NoteStore }> = (props) => {
   const noteCounter = useRef(0); //This counter uses to make unique id when we are creating new note.
-  const [text, setText] = useState("");
+  const appUI = useLocalObservable(() => ({
+    text: "",
+    setText(value: string) {
+      this.text = value;
+    },
+  }));
 
   const addNote = () => {
     const note: INote = {
       id: "id" + (props.noteStore.notes.length + noteCounter.current),
-      text,
+      text: appUI.text,
       done: false,
     };
     props.noteStore.addNote(note);
-    setText("");
+    appUI.setText("");
     noteCounter.current++;
   };
 
@@ -26,8 +32,8 @@ export const AddNewNote: React.FC<{ noteStore: NoteStore }> = (props) => {
         aria-label="Text of note"
         aria-describedby="button-addon2"
         maxLength={200}
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+        value={appUI.text}
+        onChange={(e) => appUI.setText(e.target.value)}
       />
       <div className="input-group-append">
         <button
@@ -35,7 +41,7 @@ export const AddNewNote: React.FC<{ noteStore: NoteStore }> = (props) => {
           type="button"
           id="button-addon2"
           onClick={addNote}
-          disabled={text.trim().length === 0}
+          disabled={appUI.text.trim().length === 0}
         >
           Add note
         </button>
@@ -43,4 +49,4 @@ export const AddNewNote: React.FC<{ noteStore: NoteStore }> = (props) => {
     </div>
   );
 };
-export default AddNewNote;
+export default observer(AddNewNote);
